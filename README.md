e484d95d9b82a4a75b542fad48134293ce6c4927
jingtao <jingtao@smartisan.cn>
2016-05-12 15:06:31 +0800

    add file from adbi

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 lib.jar from https://github.com/ThomasKing2014/ELF-ARM-HOOK-Library
+
 see also http://bbs.pediy.com/showthread.php?t=196228
+
+refer to http://blog.csdn.net/roland_sun/article/details/34109569
--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := doHook
-LOCAL_SRC_FILES := doHook.c
+LOCAL_SRC_FILES := doHook.c util.c hook.c epoll.c epoll_arm.c.arm
 LOCAL_LDLIBS += -llog
 include $(BUILD_SHARED_LIBRARY)
